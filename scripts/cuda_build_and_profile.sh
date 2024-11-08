647488ea782f7ff8cef7f8caeb539e95895f66c0
Bohdan Semenov <bodyasemzen@gmail.com>
2024-11-08 18:02:29 +0200

    cuda profile

--- scripts/cuda_build_and_profile.sh
+++ scripts/cuda_build_and_profile.sh
@@ -9,4 +9,5 @@ root_dir=$(realpath "$script_dir/../")
 cmake --build "$root_dir/build"
 cd "$root_dir/build/src/cuda"
 
-./cuda_app
+nsys profile --trace cuda,nvtx --cuda-memory-usage true  --force-overwrite true  --output profile_run ./cuda_app
+
